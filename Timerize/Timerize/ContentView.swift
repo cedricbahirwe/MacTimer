78461630d8211b36e555c2ce38389c16756a1435
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-06 22:38:50 +0200

    Add Project

---
+++ Timerize/Timerize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timerize
+//
+//  Created by Cédric Bahirwe on 06/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timerize/Timerize/TimerizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerizeApp.swift
+//  Timerize
+//
+//  Created by Cédric Bahirwe on 06/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TimerizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
